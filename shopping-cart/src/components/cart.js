38bfe1f4356197c34d1b0e328966d8d733afda14
longtkhd <longtkhd2709@gmail.com>
2019-11-01 23:15:10 +0700

    render order book in card when click add

--- shopping-cart/src/components/cart.js
+++ shopping-cart/src/components/cart.js
@@ -1,12 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect} from 'react-redux';
+
 
 class Cart extends React.Component {
+    
 
     render() {
+        let addedItems = this.props.items.map(item => {
+            return (
+                <div className="card mt-4" style={{ width: '18rem' }} key={item.id}>
+                    <img className="card-img-top" src={item.img} alt={item.title} />
+                    <div className="card-body">
+                        <h5 className="card-title">{item.title}</h5>
+                        <p className="card-text">{item.desc}</p>
+                        <p><b>Price: {item.price}$</b></p>
+                        <span className="btn btn-primary" onClick={() => this.handleClick(item.id)}>Add</span>
+                    </div>
+                </div>
+            )
+        })
+
+
+
         return (
-            <h1 className ="mt-5">this is card view</h1>
+            <div className="container mt-5">
+                <h3 className="center " >Your Oders</h3>
+                <div className="box">
+                    {addedItems }
+                </div>
+            </div>
         );
     }
 }
+ const mapStateToProps = (state, ownProps) => {
+    return {
+        items: state.addedItems
+    }
+}
 
-export default Cart;
+export default connect(mapStateToProps)(Cart);
